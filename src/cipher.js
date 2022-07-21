540f05e556b9d27782404ada10355d9ac9d4a0b0
AlanaK2 <alanakarine27@gmail.com>
2022-07-21 11:51:10 -0300

    .

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
-const cipher = {
-  // ...
-};
+    
+/*const cipher = function cipherEncode(texto,offset)
+{return ((texto.charCodeAt()-65+offset) % 26) + 65}
+*/
+
+ const cipher = {encode: function(texto, offset){
+    return ((texto.charCodeAt()-65+offset)% 26)+65
+    let resultado= encode(texto, offset)
+    alert(resultado)
+ }
+    
+}
+newText = (texto.fromCharCode(resultado))
+alert(newText)
 
 export default cipher;
+
+/*function letraCorrespondente (resultado){
+    return (resultado)
+}
+
+
+{
+
+};
+
+// ...
+
+/*function cipher.encode(valor,frase)
+let valor= document.querySelector('input [name="deslocamento"]:checked').value
+let frase= document.querySelector('textarea [name="texto"]:checked').value
+let ascii= frase.charCodeAt(frase)
+*/
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import cipher from './cipher.js';
+const mensagem= document.getElementById("mensagem")
+const deslocamento= document.getElementById("deslocamento")
+const btnCodificar= document.getElementById("codificar")
+const btnDecodificar= document.getElementById("decodificar")
+btnCodificar.addEventListener("click", function(){
+    const texto= mensagem.value
+    const offset= deslocamento.value
+ cipher.encode(texto,offset)
+})
+
+btnDecodificar.addEventListener("click", function(){
+    const texto= mensagem.value
+    const offset= deslocamento.value
+
+})
+
+
+/*
+codificar_element.addEventListener("click", code)= cipher.encode();
+let decodificar_element= document.getElementById("decodificar")
+decodificar_element.addEventListener("click", uncode)= cipher.decode()
+
+function validaForm() {
+    let questions = document.querySelector('textarea:checked').value;
+    if (questions =="") {alert("Escreva sua mensagem!")}
+
+}
 
 console.log(cipher);
+*/
