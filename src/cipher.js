0a63138294557af21926dcff22492e8c1a3601c9
AlanaK2 <alanakarine27@gmail.com>
2022-07-25 18:26:30 -0300

    Cifrando e decifrando frases com letras maiusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,52 +1,25 @@
+     const cipher = {encode, decode}
     
-/*const cipher = function cipherEncode(texto,offset)
-{return ((texto.charCodeAt()-65+offset) % 26) + 65}
-*/
-
- const cipher = {encode: function(texto, offset){     
-     let newText = ""
-     let code= texto.charCodeAt()   
-     if (texto=="") {
-        return alert("Escreva a sua mensagem!")
-     }
-     else {
-        return alert (newText+=String.fromCharCode(((code-65+offset)%26)+65))}
-     },
-      decode: function(texto,offset){
-        let newText= ""
-        let dec= texto.charCodeAt()
-        if (texto=="")
-         {
-        return alert("Escreva a sua mensagem!")
+ function encode(texto, offsetFormat) {     
+     let resultado= ""
+             for (let i = 0; i < texto.length; i++){
+                let code= ((texto.charCodeAt(i)-65+offsetFormat)%26)+65
+            let newText= String.fromCharCode(code)
+            resultado+=newText            
+            
+        }    
+        return resultado       
+    }
+    function decode (texto, offsetFormat){
+        let resultadoDec=""
+        for (let i = 0; i < texto.length; i++){
+            let dec= ((texto.charCodeAt(i)-65-offsetFormat)%26)+65
+            let newTextDec= String.fromCharCode(dec)
+           resultadoDec+=newTextDec
         }
-        else { return alert (newText+=String.fromCharCode(((dec-65-offset)%26)+65))
-
-        }   
-       }
-      }
-        
-    
- 
- 
-
-
-
-export default cipher;
-
-/*function letraCorrespondente (resultado){
-    return (resultado)
-}
-
-
-{
-
-};
-
-// ...
-
-/*function cipher.encode(valor,frase)
-let valor= document.querySelector('input [name="deslocamento"]:checked').value
-let frase= document.querySelector('textarea [name="texto"]:checked').value
-let ascii= frase.charCodeAt(frase)
-*/
+    return resultadoDec
+        }    
+     
+      export default cipher;
+      
 
--- src/index.js
+++ src/index.js
@@ -6,29 +6,24 @@ const btnDecodificar= document.getElementById("decodificar")
 btnCodificar.addEventListener("click", function(){
     const texto= mensagem.value
     const offset= deslocamento.value
- cipher.encode(texto,offset)
+    const offsetFormat= parseInt(offset)
+ cipher.encode(texto,offsetFormat)
+ let resultado= cipher.encode(texto,offsetFormat)
+ if (texto=="") {
+    return alert("Escreva a sua mensagem!")}
+ else {alert (resultado)};
+ })
 
-})
 
 
 btnDecodificar.addEventListener("click", function(){
     const texto= mensagem.value
     const offset= deslocamento.value
-    cipher.decode(texto,offset)
-
-})
-
-
-/*
-codificar_element.addEventListener("click", code)= cipher.encode();
-let decodificar_element= document.getElementById("decodificar")
-decodificar_element.addEventListener("click", uncode)= cipher.decode()
-
-function validaForm() {
-    let questions = document.querySelector('textarea:checked').value;
-    if (questions =="") {alert("Escreva sua mensagem!")}
-
+    const offsetFormat= parseInt(offset)
+    cipher.decode(texto,offsetFormat)
+    let resultadoDec= cipher.decode(texto,offsetFormat)
+    if (texto=="") {
+        return alert("Escreva a sua mensagem!")}
+     else {alert (resultadoDec)};    
 }
-
-console.log(cipher);
-*/
+    )
