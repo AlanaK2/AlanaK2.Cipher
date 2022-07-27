bfdcf74398d8398a8e703591aa9b1cd80e339004
AlanaK2 <alanakarine27@gmail.com>
2022-07-27 11:06:35 -0300

    Decifrando e cifrando letras maiusculas, ok em todos os testes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import cipher from './cipher.js';
-const mensagem= document.getElementById("mensagem")
-const deslocamento= document.getElementById("deslocamento")
-const btnCodificar= document.getElementById("codificar")
-const btnDecodificar= document.getElementById("decodificar")
-btnCodificar.addEventListener("click", function(){
-    const texto= mensagem.value
-    const offset= deslocamento.value
-    const offsetFormat= parseInt(offset)
- cipher.encode(texto,offsetFormat)
- let resultado= cipher.encode(texto,offsetFormat)
+const mensagem = document.getElementById("mensagem")
+const deslocamento = document.getElementById("deslocamento")
+const btnCodificar = document.getElementById("codificar")
+const btnDecodificar = document.getElementById("decodificar")
+btnCodificar.addEventListener("click", function () {
+    const texto = mensagem.value
+    const offset = deslocamento.value
+    const offsetFormat = parseInt(offset)
+    cipher.encode(offsetFormat,texto)
+    let resultado = cipher.encode(offsetFormat, texto)
  if (texto=="") {
     return alert("Escreva a sua mensagem!")}
- else {alert (resultado)};
- })
+ else document.getElementById("resultado").innerHTML=resultado;
+ 
+})
 
 
 
-btnDecodificar.addEventListener("click", function(){
-    const texto= mensagem.value
-    const offset= deslocamento.value
-    const offsetFormat= parseInt(offset)
-    cipher.decode(texto,offsetFormat)
-    let resultadoDec= cipher.decode(texto,offsetFormat)
-    if (texto=="") {
-        return alert("Escreva a sua mensagem!")}
-     else {alert (resultadoDec)};    
+btnDecodificar.addEventListener("click", function () {
+    const texto = mensagem.value
+    const offset = deslocamento.value
+    const offsetFormat = parseInt(offset)
+    cipher.decode(offsetFormat,texto)
+    let resultadoDec = cipher.decode(offsetFormat, texto)
+    if (texto == "") {
+        return alert("Escreva a sua mensagem!")
+    }
+    else document.getElementById("resultado").innerHTML=resultadoDec;
 }
-    )
+)
